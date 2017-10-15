5a49a4b6ca1d2ce72d2b23b34ba6ea1c0445e65e
Kseniya Shustova <shustovakv@mail.ru>
2017-10-16 00:25:58 +0300

    JavaDoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
 
+/**
+ *Class Calculate решение задачи части 001 урок 1.
+ *@author shustovakv
+ *@since 15.10.2017
+ */
 public class Calculate {
+	/**
+	* Вывод текста на экран.
+	* @param "Hello world." выводится на экран.
+	*/
 	public static void main (String[] args) {
 		System.out.println("Hello world.");
 	}
