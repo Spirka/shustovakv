bbb0124e1dce52b49c448f5076c3ecd33480c1c4
Kseniya Shustova <shustovakv@mail.ru>
2017-10-17 01:49:01 +0300

    format Calculate.java

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -11,6 +11,7 @@ public class Calculate {
 	* @param args - args.
 	*/
 	public static void main(String[] args) {
+
 		System.out.println("Hello World");
 	}
 	/**
@@ -19,6 +20,7 @@ public class Calculate {
 	* @return Echo plus your name.
 	*/
 	public String echo(String name) {
+
 		return "Echo, echo, echo : " + name;
 	}
 }
