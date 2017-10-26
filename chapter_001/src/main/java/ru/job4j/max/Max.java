a53e3d60af15f358739faaf657ab13ee9da6ea5c
Kseniya Shustova <shustovakv@mail.ru>
2017-10-26 23:03:04 +0300

    Max

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -2,17 +2,20 @@ package ru.job4j.max;
 /**
  *Class Max.
  *@author shustovakv
- *@since 19.10.2017
+ *@since 26.10.2017
  */
 public class Max {
 	/**
      * max.
-     * @param first переменная может быть больше или меньше second, third.
-     * @param second переменная может быть больше или меньше first, third.
-	 * @param third переменная может быть больше или меньше first, second. 
+     * @param first переменная может быть больше или меньше second.
+     * @param second переменная может быть больше или меньше first.
 	 * @return result.
      */
+	public int max(int first, int second) {
+		int result = (first > second) ? first : second;
+		return result;
+	}
 	public int max(int first, int second, int third) {
-		return Math.max(first, Math.max(second, third));
+		return max(max(first, second), third);
 	}
 }
