bfadcd9daf20dfd490671552e5379c5da6e2c76a
Kseniya Shustova <shustovakv@mail.ru>
2017-11-13 23:40:21 +0300

    5.2. Создание программы поворота квадратного массива. Усовершенствованная версия.

--- chapter_001/src/main/java/ru/job4j/array/RotateArray.java
+++ chapter_001/src/main/java/ru/job4j/array/RotateArray.java
@@ -12,8 +12,7 @@ public class RotateArray {
      * @return перевернутый на 90 градусов массив.
      */
     public int[][] rotate(int[][] array) {
-        if (array.length % 2 == 0) { // условие четности
-            for (int i = 0; i < array.length / 2; i++) {
+        for (int i = 0; i < (array.length % 2 + array.length / 2); i++) {
                 for (int j = 0; j < array.length / 2; j++) {
                     int temp = array[i][j];
                     array[i][j] = array[array.length - 1 - j][i];
@@ -22,19 +21,6 @@ public class RotateArray {
                     array[j][array.length - 1 - i] = temp;
                 }
             }
-            return array;
-
-        } else {
-            for (int i = 0; i < (array.length / 2 + 1); i++) {
-                for (int j = 0; j < (array.length / 2); j++) {
-                    int temp = array[i][j];
-                    array[i][j] = array[array.length - 1 - j][i];
-                    array[array.length - 1 - j][i] = array[array.length - 1 - i][array.length - 1 - j];
-                    array[array.length - 1 - i][array.length - 1 - j] = array[j][array.length - 1 - i];
-                    array[j][array.length - 1 - i] = temp;
-                }
-            }
-            return array;
-        }
+        return array;
     }
 }
