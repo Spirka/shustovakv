159affd864eb577ccfa82a18caedc2f69778a706
Kseniya Shustova <shustovakv@mail.ru>
2017-11-19 23:04:57 +0300

    Проверка, что одно слово находится в другом слове [#228] Исправлено

--- chapter_001/src/main/java/ru/job4j/array/Content.java
+++ chapter_001/src/main/java/ru/job4j/array/Content.java
@@ -1,7 +1,9 @@
 package ru.job4j.array;
+
 /**
  * Class Content.
- * @author  shustovakv
+ *
+ * @author shustovakv
  * @since 14.11.2017
  */
 public class Content {
@@ -18,17 +20,18 @@ public class Content {
         boolean contains = true;
 
         for (int i = 0; i < charsOrigin.length; i++) {
-            for (int j = 0; j < charsSub.length;) {
+            for (int j = 0; j < charsSub.length; j++) {
                 if (charsOrigin[i] == charsSub[j]) {
                     boolean flag = true;
                     break;
                 } else {
                     if (charsSub[j] != charsOrigin[j + i]) {
-                    contains = false;
-                    break;
+                        contains = false;
+                        break;
                     }
                 }
             }
-        } return contains;
+        }
+        return contains;
     }
 }
