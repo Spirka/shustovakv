d7117288f7a1e5f100f631aef90a32468e6f89a4
Kseniya Shustova <shustovakv@mail.ru>
2017-11-19 15:21:56 +0300

    Проверка, что одно слово находится в другом слове [#228] Исправлено

--- chapter_001/src/main/java/ru/job4j/array/Content.java
+++ chapter_001/src/main/java/ru/job4j/array/Content.java
@@ -26,9 +26,8 @@ public class Content {
                     if (charsSub[j] != charsOrigin[j + i]) {
                     contains = false;
                     break;
+                    }
                 }
-                return contains;
-            }
             }
         } return contains;
     }
