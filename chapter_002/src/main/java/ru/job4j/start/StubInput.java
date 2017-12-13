5f1caa7b3b667f6b50e7b328e7b846dfc814b387
Kseniya Shustova <shustovakv@mail.ru>
2017-12-13 12:10:59 +0300

    1. Реализовать события на внутренних классах. [#787]

--- chapter_002/src/main/java/ru/job4j/start/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/start/StubInput.java
@@ -10,11 +10,17 @@ public class StubInput implements Input {
     private String[] answers;
     private int position = 0;
 
-    public StubInput(String[] answers) {
+    StubInput(String[] answers) {
         this.answers = answers;
     }
 
+    @Override
     public String ask(String question) {
         return answers[position++];
+    }
+
+    @Override
+    public int ask(String question, int[] range) {
+        return Integer.valueOf(answers[position++]);
     }
 }
--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import ru.job4j.start.MenuOutException;
+
 import java.util.*;
 
 /**
@@ -9,8 +11,26 @@ import java.util.*;
  */
 public class ConsoleInput implements Input {
     private Scanner scanner = new Scanner(System.in);
+
     public String ask(String question) {
         System.out.print(question);
         return scanner.nextLine();
+    }
+
+    @Override
+    public int ask(String question, int[] range) {
+        int key = Integer.valueOf(this.ask(question));
+        boolean exist = false;
+        for (int value : range) {
+            if (value == key) {
+                exist = true;
+                break;
+            }
+        }
+        if (exist) {
+            return key;
+        } else {
+            throw new MenuOutException("Out of menu range");
+        }
     }
 }
