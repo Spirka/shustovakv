e12c79b6245bb8499d00df1b07f1d59132167344
Kseniya Shustova <shustovakv@mail.ru>
2018-01-28 00:50:46 +0300

    Turn. Рефакторинг.

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -12,19 +12,11 @@ public class Turn {
      * @return перевернутый массив.
      */
     public int[] back(int[] array) {
-        if (array == null || array.length % 2 != 0) {
-            for (int i = 0; i <= array.length / 2; i++) {
+            for (int i = 0; i < (array.length % 2 + array.length / 2); i++) {
                 int temp = array[i];
                 array[i] = array[array.length - 1 - i];
                 array[array.length - 1 - i] = temp;
             }
-        } else {
-            for (int i = 0; i < array.length / 2; i++) {
-                int temp = array[i];
-                array[i] = array[array.length - 1 - i];
-                array[array.length - 1 - i] = temp;
-            }
-        }
-        return array;
+            return array;
     }
 }
