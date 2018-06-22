42e775d3da5ba41467db27bd07c787e869063baf
Spirka <shustovakv@mail.ru>
2018-06-22 15:18:04 +0300

    PrimeIterator[#151] JavaDocs

--- chapter_004/src/main/java/ru.job4j/iterator/PrimeIterator.java
+++ chapter_004/src/main/java/ru.job4j/iterator/PrimeIterator.java
@@ -3,16 +3,37 @@ package ru.job4j.iterator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Class EvenNumbersIterator.
+ *
+ * @author shustovakv
+ * @since 18.06.2018
+ */
+
 public class PrimeIterator implements Iterator<Integer> {
 
+    /**
+     * Field List of arbitrary numbers.
+     */
     private final int[] numbers;
+
+    /**
+     * Field position.
+     */
     private int index = 0;
 
+    /**
+     * Constructor
+     * @param numbers List of arbitrary numbers.
+     */
     public PrimeIterator(final int[] numbers) {
         this.numbers = numbers;
     }
 
-
+    /**
+     * Method hasNext
+     * @return true only if the array has prime numbers before the pointer.
+     */
     @Override
     public boolean hasNext() {
         boolean result = false;
@@ -26,6 +47,10 @@ public class PrimeIterator implements Iterator<Integer> {
         return result;
     }
 
+    /**
+     * Method next
+     * @return only prime numbers
+     */
     @Override
     public Integer next() {
         if (!this.hasNext()) {
@@ -34,6 +59,11 @@ public class PrimeIterator implements Iterator<Integer> {
         return this.numbers[index++];
     }
 
+    /**
+     * Method IsPrime
+     * @param number arbitrary number
+     * @return true only if the number is prime
+     */
     private boolean isPrime(int number) {
         boolean result = true;
         if (number > 1) {
