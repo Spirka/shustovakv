bc8305874cf24a0a0a143ee6b011c150ac1cf5d2
Spirka <shustovakv@mail.ru>
2018-09-14 11:47:11 +0300

    Перекрывать и equals и hashCode [#1002]

--- chapter_004/src/main/java/ru/job4j/map/User.java
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -15,6 +15,13 @@ public class User {
     private Calendar birthday;
 
     @Override
+    public int hashCode() {
+
+        return Objects.hash(name, children, birthday);
+    }
+
+    @Override
+
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
