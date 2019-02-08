fed194fe9345fccc73d4b0d2ca5c007f4eafe385
Spirka <shustovakv@mail.ru>
2019-02-08 19:38:42 +0300

    Архивировать проект. [#861]

--- chapter_005/src/main/java/ru/job4j/threads/PackZip.java
+++ chapter_005/src/main/java/ru/job4j/threads/PackZip.java
@@ -1,7 +1,11 @@
 package ru.job4j.threads;
 
 import java.io.*;
+import java.net.URI;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Queue;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -12,38 +16,46 @@ import java.util.zip.ZipOutputStream;
  */
 public class PackZip {
 
-    public void zip(Args args) throws IOException {
+    public void zip(Args args) {
         String zip_file = args.output();
-        ZipOutputStream zout = null;
-        try {
-            zout = new ZipOutputStream(new FileOutputStream(args.directory() + "//" + zip_file));
-        } catch (FileNotFoundException e) {
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(args.directory() + "//" + zip_file))) {
+            File fileSource = new File(args.directory());
+            List<String> exc = args.exclude();
+            this.addDirectory(zout, fileSource, exc);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        File fileSource = new File(args.directory());
-        List<String> exc = args.exclude();
-        this.addDirectory(zout, fileSource, exc);
     }
 
     private void addDirectory(ZipOutputStream zout, File fileSource, List<String> exc) throws IOException {
+        Queue<File> queue = new LinkedList<>();
+        queue.offer(fileSource);
+        URI base = fileSource.toURI();
+        while (!queue.isEmpty()) {
+            File[] files = Objects.requireNonNull(queue.poll()).listFiles();
+            assert files != null;
+            for (int i = 0; i < files.length; i++) {
+                final File file = files[i];
+                String name = base.relativize(file.toURI()).getPath();
+                if (file.isDirectory()) {
+                    name = name.endsWith("/") ? name : name + "/";
+                    zout.putNextEntry(new ZipEntry(name));
+                    ((LinkedList<File>) queue).addFirst(file);
+                } else if (exclude(file, exc) && !(file.getName().contains(".zip"))) {
+                    FileInputStream fis = new FileInputStream(file);
+                    zout.putNextEntry(new ZipEntry(name));
 
-        File[] files = fileSource.listFiles();
-        for (int i = 0; i < files.length; i++) {
-            if (files[i].isDirectory()) {
-                addDirectory(zout, files[i],exc);
-                continue;
-            } if (exclude(files[i], exc)) {
-                FileInputStream fis = new FileInputStream(files[i]);
-                zout.putNextEntry(new ZipEntry(files[i].getPath()));
-
-                byte[] buffer = new byte[2048];
-                int length;
-                while((length = fis.read(buffer)) > 0) {
-                    zout.write(buffer, 0, length);
+                    byte[] buffer = new byte[2048];
+                    int length;
+                    while((length = fis.read(buffer)) > 0) {
+                        zout.write(buffer, 0, length);
+                    }
+                    zout.closeEntry();
+                    fis.close();
                 }
-                zout.closeEntry();
-                fis.close();
-
             }
         }
     }
