d278b5f38159a7b964c49df7407d8ad02f76bbb5
Spirka <shustovakv@mail.ru>
2019-03-26 11:36:36 +0300

    Поиск файлов по критерию [#783] with tests

--- chapter_005/src/test/java/ru/job4j/finder/FileFinderTest.java
+++ chapter_005/src/test/java/ru/job4j/finder/FileFinderTest.java
@@ -14,43 +14,61 @@ import static org.junit.Assert.*;
 
 public class FileFinderTest {
 
-    private static final String TEST_DIR = System.getProperty("java.io.tmpdir") + "TEST_DIR";
-
     @Before
     public void setUp() throws Exception {
-        new File(TEST_DIR).mkdirs();
-        new File(TEST_DIR + "/test1").mkdirs();
-        new File(TEST_DIR + "/test2").mkdirs();
-        File file = new File(TEST_DIR + "/test1/test.txt");
-        File file1 = new File(TEST_DIR + "/test2/weather.jpg");
-        file.createNewFile();
-        file1.createNewFile();
+        String path = System.getProperty("java.io.tmpdir");
+        String parent = path + "/TEST_DIR";
+        new File(parent).mkdirs();
+        new File(parent + "/test1").mkdirs();
+        new File(parent + "/test2").mkdirs();
+        new File(parent + "/test1/test.txt").createNewFile();
+        new File(parent + "/test2/weather.jpg").createNewFile();
     }
 
-    @Test
     public void testFinder(String[] args, String expected) throws IOException {
         FileFinder.main(args);
         String readLine;
-        try (BufferedReader br = new BufferedReader(new FileReader(TEST_DIR + "/log.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(
+                System.getProperty("java.io.tmpdir") + "/TEST_DIR/log.txt"))) {
             readLine = br.readLine();
         }
         assertThat(readLine, is(expected));
     }
 
     @Test
-    public void whenFileFinderToFindFileByName() throws IOException {
-        String[] args = {"-jar", "FileFinder.jar", "-d", TEST_DIR, "-n", "test.txt", "-f", "-o", TEST_DIR + "/log.txt"};
-        String expected = TEST_DIR + "\\test1\\test.txt";
+    public void whenFileFinderFindFileByNameThenItRecordedInLog() throws IOException {
+        String[] args = {"-jar", "FileFinder.jar", "-d",
+                System.getProperty("java.io.tmpdir") + "/TEST_DIR", "-n", "test.txt", "-f", "-o",
+                System.getProperty("java.io.tmpdir") + "/TEST_DIR/log.txt"};
+        String expected = System.getProperty("java.io.tmpdir") + "TEST_DIR" + "/test1/test.txt";
         testFinder(args, expected);
     }
 
+    @Test
+    public void whenFileFinderFindFileByMaskThenItRecordedInLog() throws IOException {
+        String[] args = {"-jar", "FileFinder.jar", "-d",
+                System.getProperty("java.io.tmpdir") + "/TEST_DIR", "-n", "*.jpg", "-m", "-o",
+                System.getProperty("java.io.tmpdir") + "/TEST_DIR" + "/log.txt"};
+        String expected = System.getProperty("java.io.tmpdir") + "TEST_DIR" + "/test2/weather.jpg";
+        testFinder(args, expected);
+    }
+
+    @Test
+    public void whenFileFinderFindFileByRegExThenItRecordedInLog() throws IOException {
+        String[] args = {"-jar", "FileFinder.jar", "-d",
+                System.getProperty("java.io.tmpdir") + "/TEST_DIR", "-n", "(weather).*", "-r", "-o",
+                System.getProperty("java.io.tmpdir") + "/TEST_DIR" + "/log.txt"};
+        String expected = System.getProperty("java.io.tmpdir") + "TEST_DIR" + "/test2/weather.jpg";
+        testFinder(args, expected);
+    }
+/**
     @After
     public void tearDown() {
-        new File(TEST_DIR + "/test", "test.txt").delete();
-        new File(TEST_DIR + "/test2", "weather").delete();
-        new File(TEST_DIR + "/log.txt").delete();
-        new File(TEST_DIR + "/test1" ).delete();
-        new File(TEST_DIR + "/test2" ).delete();
-        new File(System.getProperty("java.io.tmpdir") + "/TEST_DIR").delete();
-    }
+        new File(System.getProperty("java.io.tmpdir") + "TEST_DIR" + "/test1", "test.txt").delete();
+        new File(System.getProperty("java.io.tmpdir") + "TEST_DIR" + "/test2", "weather.jpg").delete();
+        new File(System.getProperty("java.io.tmpdir") + "TEST_DIR" + "/log.txt").delete();
+        new File(System.getProperty("java.io.tmpdir") + "TEST_DIR" + "/test1").delete();
+        new File(System.getProperty("java.io.tmpdir") + "TEST_DIR" + "/test2").delete();
+        new File(System.getProperty("java.io.tmpdir") + "TEST_DIR").delete();
+    }*/
 }
