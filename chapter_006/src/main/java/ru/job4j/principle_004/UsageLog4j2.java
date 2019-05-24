295b026e208fc42d1a6e9b39ed5c4bfd9a6fbd28
Spirka <shustovakv@mail.ru>
2019-05-24 11:32:13 +0300

    Log4j 2. Логирование системы. [#95335]

---
+++ chapter_006/src/main/java/ru/job4j/principle_004/UsageLog4j2.java
@@ -0,0 +1,22 @@
+package ru.job4j.principle_004;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+/**
+ * Class UsageLog4j2.
+ *
+ * @author shustovakv
+ * @since 24.05.2019
+ */
+public class UsageLog4j2 {
+    private static final Logger LOG = LogManager.getLogger(UsageLog4j2.class.getName());
+
+    public static void main(String[] args) {
+        LOG.trace("trace message");
+        LOG.debug("debug message");
+        LOG.info("info message");
+        LOG.warn("warn message");
+        LOG.error("error message");
+    }
+}
