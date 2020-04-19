b2eb65f518e1e3515c994571eccbb782794b1f28
Kseniya Dergunova <kdergunova@vocalize.ru>
2020-04-19 17:00:47 +0300

    Шаблонизатор. [#855]

---
+++ chapter_009/src/main/java/ru/job4j/template/SomeGenerator.java
@@ -0,0 +1,16 @@
+package ru.job4j.template;
+
+import java.util.Map;
+
+/**
+ * Class SomeGenerator
+ *
+ * @author Kseniya Dergunova
+ * @since 19.04.2020
+ */
+public class SomeGenerator implements Generator {
+    @Override
+    public String produce(String template, Map<String, String> args) {
+        return null;
+    }
+}
